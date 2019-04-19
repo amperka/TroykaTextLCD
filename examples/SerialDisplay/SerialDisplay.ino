b590b25ac243e85be13d831795a6f68865e1307d
Igor89 <igor@amperka.ru>
2019-04-19 19:11:23 +0300

    init library

---
+++ examples/SerialDisplay/SerialDisplay.ino
@@ -0,0 +1,30 @@
+// библиотека для работы с дисплеем
+#include <TroykaTextLCD.h>
+
+// создаем объект для работы с дисплеем
+TroykaTextLCD lcd;
+
+void setup() {
+  // открываем монитор Serial-порта
+  Serial.begin(9600);
+  // устанавливаем количество столбцов и строк экрана
+  lcd.begin(16, 2);
+  // устанавливаем контрастность
+  lcd.setContrast(27);
+  // устанавливаем яркость
+  lcd.setBrightness(255);
+
+}
+
+void loop() {
+  // если пришли данные из Serial-порта
+  if (Serial.available()) {
+    // очищаем экран
+    lcd.clear();
+    // ожидаем все данные
+    while (Serial.available() > 0) {
+      // выводим все пришедшие данные на экран
+      lcd.write(Serial.read());
+    }
+  }
+}
