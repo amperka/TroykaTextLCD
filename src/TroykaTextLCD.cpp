09243da8d68b0c02c8ae9848e35a323cdbbcacbb
Igor89 <igor@amperka.ru>
2019-11-06 16:15:39 +0300

    add support ESP8266 and Arduino ARM

--- src/TroykaTextLCD.cpp
+++ src/TroykaTextLCD.cpp
@@ -1,173 +1,179 @@
 #include "TroykaTextLCD.h"
 
-TroykaTextLCD::TroykaTextLCD(uint8_t pinBacklight, uint8_t i2cAddress, TwoWire* wire) {
-    _pinBacklight = pinBacklight;
-    _i2cAddress = i2cAddress;
-    _wire = wire;
+TroykaTextLCD::TroykaTextLCD(TwoWire *wire, uint8_t i2cAddress,
+                             uint8_t pinBacklight) {
+  _wire = wire;
+  _i2cAddress = i2cAddress;
+  _pinBacklight = pinBacklight;
 }
 
 void TroykaTextLCD::begin(uint8_t cols, uint8_t rows, uint8_t charSize) {
-    _cols = cols;
-    _rows = rows;
-    _charSize = charSize;
-    pinMode(_pinBacklight, OUTPUT);
-    setBrightness(255);
+  _cols = cols;
+  _rows = rows;
+  _charSize = charSize;
+  pinMode(_pinBacklight, OUTPUT);
+  setBrightness(255);
 
-    _wire->begin();
-    delay(40);
+  _wire->begin();
+  delay(40);
 
-    _displayFunction = LCD_8BITMODE;
-    if (_rows == 1) {
-        _displayFunction |= LCD_1LINE;
-    } else if (_rows == 2) {
-        _displayFunction |= LCD_2LINE;
-    }
-    if (charSize == 1) {
-        _displayFunction |= LCD_5x8DOTS;
-    } else if ((charSize == 2) && (_rows == 2)) {
-        _displayFunction |= LCD_5x10DOTS;
-    }
-    setNormalFunction();
-    setExtendFunction();
-    command(LCD_EX_SETBIASOSC | LCD_BIAS_1_5 | LCD_OSC_183HZ);
-    command(LCD_EX_FOLLOWERCONTROL | LCD_FOLLOWER_ON | LCD_RAB_2_00);
-    delay(200);
-    setNormalFunction();
+  _displayFunction = LCD_8BITMODE;
+  if (_rows == 1) {
+    _displayFunction |= LCD_1LINE;
+  } else if (_rows == 2) {
+    _displayFunction |= LCD_2LINE;
+  }
+  if (charSize == 1) {
+    _displayFunction |= LCD_5x8DOTS;
+  } else if ((charSize == 2) && (_rows == 2)) {
+    _displayFunction |= LCD_5x10DOTS;
+  }
+  setNormalFunction();
+  setExtendFunction();
+  command(LCD_EX_SETBIASOSC | LCD_BIAS_1_5 | LCD_OSC_183HZ);
+  command(LCD_EX_FOLLOWERCONTROL | LCD_FOLLOWER_ON | LCD_RAB_2_00);
+  delay(200);
+  setNormalFunction();
 
-    _displayControl = 0x00;
-    _displayControl = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;
-    command(LCD_DISPLAYCONTROL | _displayControl);
-    
-    clear();
+  _displayControl = 0x00;
+  _displayControl = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;
+  command(LCD_DISPLAYCONTROL | _displayControl);
 
-    _displayMode = 0x00;
-    _displayMode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
-    command(LCD_ENTRYMODESET | _displayMode);
+  clear();
+
+  _displayMode = 0x00;
+  _displayMode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
+  command(LCD_ENTRYMODESET | _displayMode);
 }
 
 void TroykaTextLCD::setContrast(uint8_t contrast) {
-    setExtendFunction();
-    command(LCD_EX_CONTRASTSETL | (contrast & 0x0f));
-    command(LCD_EX_POWICONCONTRASTH | LCD_ICON_ON | LCD_BOOST_ON | ((contrast >> 4) & 0x03));
-    setNormalFunction();
+  setExtendFunction();
+  command(LCD_EX_CONTRASTSETL | (contrast & 0x0f));
+  command(LCD_EX_POWICONCONTRASTH | LCD_ICON_ON | LCD_BOOST_ON |
+          ((contrast >> 4) & 0x03));
+  setNormalFunction();
 }
 
-void TroykaTextLCD::setBrightness(uint8_t brightness) {
+void TroykaTextLCD::setBrightness(uint16_t brightness) {
+  // Default PWM resolution 10 bit for board ESP8266
+  // Default PWM resolution 8 bit other boards
+  #if defined(ESP8266)
+    analogWrite(_pinBacklight, brightness << 2);
+  #else
     analogWrite(_pinBacklight, brightness);
+  #endif
 }
 
 void TroykaTextLCD::setIcon(uint8_t addr, uint8_t bit) {
-    setExtendFunction();
-    command(LCD_EX_SETICONRAMADDR | (addr & 0x0f));
-    write(bit);
-    setNormalFunction();
+  setExtendFunction();
+  command(LCD_EX_SETICONRAMADDR | (addr & 0x0f));
+  write(bit);
+  setNormalFunction();
 }
 
 void TroykaTextLCD::clear() {
-    command(LCD_CLEARDISPLAY);
-    delayMicroseconds(2000);
+  command(LCD_CLEARDISPLAY);
+  delayMicroseconds(2000);
 }
 
 void TroykaTextLCD::home() {
-    command(LCD_RETURNHOME);
-    delayMicroseconds(2000);
+  command(LCD_RETURNHOME);
+  delayMicroseconds(2000);
 }
 
 void TroykaTextLCD::setCursor(uint8_t col, uint8_t row) {
-    const int rowOffsets[] = {0x00, 0x40, 0x14, 0x54};
-    if(row > _rows) {
-        row = _rows - 1;
-    }
-    command(LCD_SETDDRAMADDR | (col + rowOffsets[row]));
+  const int rowOffsets[] = {0x00, 0x40, 0x14, 0x54};
+  if (row > _rows) {
+    row = _rows - 1;
+  }
+  command(LCD_SETDDRAMADDR | (col + rowOffsets[row]));
 }
 
 void TroykaTextLCD::noDisplay() {
-    _displayControl &= ~LCD_DISPLAYON;
-    command(LCD_DISPLAYCONTROL | _displayControl);
+  _displayControl &= ~LCD_DISPLAYON;
+  command(LCD_DISPLAYCONTROL | _displayControl);
 }
 
 void TroykaTextLCD::display() {
-    _displayControl |= LCD_DISPLAYON;
-    command(LCD_DISPLAYCONTROL | _displayControl);
+  _displayControl |= LCD_DISPLAYON;
+  command(LCD_DISPLAYCONTROL | _displayControl);
 }
 
 void TroykaTextLCD::noCursor() {
-    _displayControl &= ~LCD_CURSORON;
-    command(LCD_DISPLAYCONTROL | _displayControl);
+  _displayControl &= ~LCD_CURSORON;
+  command(LCD_DISPLAYCONTROL | _displayControl);
 }
 
 void TroykaTextLCD::cursor() {
-    _displayControl |= LCD_CURSORON;
-    command(LCD_DISPLAYCONTROL | _displayControl);
+  _displayControl |= LCD_CURSORON;
+  command(LCD_DISPLAYCONTROL | _displayControl);
 }
 
 void TroykaTextLCD::noBlink() {
-    _displayControl &= ~LCD_BLINKON;
-    command(LCD_DISPLAYCONTROL | _displayControl);
+  _displayControl &= ~LCD_BLINKON;
+  command(LCD_DISPLAYCONTROL | _displayControl);
 }
 
 void TroykaTextLCD::blink() {
-    _displayControl |= LCD_BLINKON;
-    command(LCD_DISPLAYCONTROL | _displayControl);
+  _displayControl |= LCD_BLINKON;
+  command(LCD_DISPLAYCONTROL | _displayControl);
 }
 
 void TroykaTextLCD::scrollDisplayLeft(void) {
-    command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVELEFT);
+  command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVELEFT);
 }
 
 void TroykaTextLCD::scrollDisplayRight(void) {
-    command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVERIGHT);
+  command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVERIGHT);
 }
 
 void TroykaTextLCD::leftToRight(void) {
-    _displayMode |= LCD_ENTRYLEFT;
-    command(LCD_ENTRYMODESET | _displayMode);
+  _displayMode |= LCD_ENTRYLEFT;
+  command(LCD_ENTRYMODESET | _displayMode);
 }
 
 void TroykaTextLCD::rightToLeft(void) {
-    _displayMode &= ~LCD_ENTRYLEFT;
-    command(LCD_ENTRYMODESET | _displayMode);
+  _displayMode &= ~LCD_ENTRYLEFT;
+  command(LCD_ENTRYMODESET | _displayMode);
 }
 
 void TroykaTextLCD::autoscroll(void) {
-    _displayMode |= LCD_ENTRYSHIFTINCREMENT;
-    command(LCD_ENTRYMODESET | _displayMode);
+  _displayMode |= LCD_ENTRYSHIFTINCREMENT;
+  command(LCD_ENTRYMODESET | _displayMode);
 }
 
 void TroykaTextLCD::noAutoscroll(void) {
-    _displayMode &= ~LCD_ENTRYSHIFTINCREMENT;
-    command(LCD_ENTRYMODESET | _displayMode);
+  _displayMode &= ~LCD_ENTRYSHIFTINCREMENT;
+  command(LCD_ENTRYMODESET | _displayMode);
 }
 
 void TroykaTextLCD::createChar(uint8_t location, uint8_t charMap[]) {
-    location &= 0x7;
-    command(LCD_SETCGRAMADDR | (location << 3));
-    for (int i = 0; i < 8; i++) {
-        write(charMap[i]);
-    }
+  location &= 0x7;
+  command(LCD_SETCGRAMADDR | (location << 3));
+  for (int i = 0; i < 8; i++) {
+    write(charMap[i]);
+  }
 }
 
-void TroykaTextLCD::command(uint8_t value) {
-    send(value, 0x00);
-}
+void TroykaTextLCD::command(uint8_t value) { send(value, 0x00); }
 
 size_t TroykaTextLCD::write(uint8_t value) {
-    send(value, 0x40);
-    return 1;
+  send(value, 0x40);
+  return 1;
 }
 
 void TroykaTextLCD::send(uint8_t value, uint8_t mode) {
-    _wire->beginTransmission(_i2cAddress);
-    _wire->write(mode);
-    _wire->write(value);
-    _wire->endTransmission();
-    delayMicroseconds(27);
+  _wire->beginTransmission(_i2cAddress);
+  _wire->write(mode);
+  _wire->write(value);
+  _wire->endTransmission();
+  delayMicroseconds(27);
 }
 
 void TroykaTextLCD::setNormalFunction() {
-    command(LCD_FUNCTIONSET | _displayFunction | LCD_NM_INSTRUCTION);
+  command(LCD_FUNCTIONSET | _displayFunction | LCD_NM_INSTRUCTION);
 }
 
 void TroykaTextLCD::setExtendFunction() {
-    command(LCD_FUNCTIONSET | _displayFunction | LCD_EX_INSTRUCTION);
+  command(LCD_FUNCTIONSET | _displayFunction | LCD_EX_INSTRUCTION);
 }
--- src/TroykaTextLCD.h
+++ src/TroykaTextLCD.h
@@ -2,141 +2,143 @@
 #define __TROYKA_TEXT_LCD_H__
 
 #include <Arduino.h>
-#include <stdint.h>
 #include <Print.h>
 #include <Wire.h>
+#include <stdint.h>
 
-#define DEFAULT_LCD_I2C_ADDR    0x3E
-#define DEFAULT_PIN_BACKLIGHT   7
+#define DEFAULT_LCD_I2C_ADDR 0x3E
+#define DEFAULT_PIN_BACKLIGHT 7
 
 // commands
-#define LCD_CLEARDISPLAY        0x01
-#define LCD_RETURNHOME          0x02
-#define LCD_ENTRYMODESET        0x04
-#define LCD_DISPLAYCONTROL      0x08
-#define LCD_CURSORSHIFT         0x10
-#define LCD_FUNCTIONSET         0x20
-#define LCD_SETCGRAMADDR        0x40
-#define LCD_SETDDRAMADDR        0x80
+#define LCD_CLEARDISPLAY 0x01
+#define LCD_RETURNHOME 0x02
+#define LCD_ENTRYMODESET 0x04
+#define LCD_DISPLAYCONTROL 0x08
+#define LCD_CURSORSHIFT 0x10
+#define LCD_FUNCTIONSET 0x20
+#define LCD_SETCGRAMADDR 0x40
+#define LCD_SETDDRAMADDR 0x80
 
 // flags for display entry mode (LCD_ENTRYMODESET)
-#define LCD_ENTRYRIGHT          0x00
-#define LCD_ENTRYLEFT           0x02
+#define LCD_ENTRYRIGHT 0x00
+#define LCD_ENTRYLEFT 0x02
 #define LCD_ENTRYSHIFTINCREMENT 0x01
 #define LCD_ENTRYSHIFTDECREMENT 0x00
 
 // flags for display on/off control (LCD_DISPLAYCONTROL)
-#define LCD_DISPLAYON           0x04
-#define LCD_DISPLAYOFF          0x00
-#define LCD_CURSORON            0x02
-#define LCD_CURSOROFF           0x00
-#define LCD_BLINKON             0x01
-#define LCD_BLINKOFF            0x00
+#define LCD_DISPLAYON 0x04
+#define LCD_DISPLAYOFF 0x00
+#define LCD_CURSORON 0x02
+#define LCD_CURSOROFF 0x00
+#define LCD_BLINKON 0x01
+#define LCD_BLINKOFF 0x00
 
 // flags for display/cursor shift (LCD_CURSORSHIFT)
-#define LCD_DISPLAYMOVE         0x08
-#define LCD_CURSORMOVE          0x00
-#define LCD_MOVERIGHT           0x04
-#define LCD_MOVELEFT            0x00
+#define LCD_DISPLAYMOVE 0x08
+#define LCD_CURSORMOVE 0x00
+#define LCD_MOVERIGHT 0x04
+#define LCD_MOVELEFT 0x00
 
 // flags for function set (LCD_FUNCTIONSET)
-#define LCD_8BITMODE            0x10
-#define LCD_4BITMODE            0x00
-#define LCD_2LINE               0x08
-#define LCD_1LINE               0x00
-#define LCD_5x10DOTS            0x04
-#define LCD_5x8DOTS             0x00
-// Normal or Extension instruction select 
-#define LCD_NM_INSTRUCTION      0x00
-#define LCD_EX_INSTRUCTION      0x01
+#define LCD_8BITMODE 0x10
+#define LCD_4BITMODE 0x00
+#define LCD_2LINE 0x08
+#define LCD_1LINE 0x00
+#define LCD_5x10DOTS 0x04
+#define LCD_5x8DOTS 0x00
+// Normal or Extension instruction select
+#define LCD_NM_INSTRUCTION 0x00
+#define LCD_EX_INSTRUCTION 0x01
 
 // bias selection / Internal OSC frequency adjust
-#define LCD_EX_SETBIASOSC       0x10
+#define LCD_EX_SETBIASOSC 0x10
 // set ICON RAM address
-#define LCD_EX_SETICONRAMADDR   0x40
+#define LCD_EX_SETICONRAMADDR 0x40
 // power / ICON control / Contrast set (high byte)
 #define LCD_EX_POWICONCONTRASTH 0x50
 // follower control
-#define LCD_EX_FOLLOWERCONTROL  0x60
+#define LCD_EX_FOLLOWERCONTROL 0x60
 // contrast set (low byte)
-#define LCD_EX_CONTRASTSETL     0x70
+#define LCD_EX_CONTRASTSETL 0x70
 
 // flags for Bias selection
 // bias will be 1/4
-#define LCD_BIAS_1_4            0x08
+#define LCD_BIAS_1_4 0x08
 // bias will be 1/5
-#define LCD_BIAS_1_5            0x00
+#define LCD_BIAS_1_5 0x00
 
 // flags Power / ICON control / Contrast set(high byte)
 // ICON display on
-#define LCD_ICON_ON             0x08
+#define LCD_ICON_ON 0x08
 // ICON display off
-#define LCD_ICON_OFF            0x00
+#define LCD_ICON_OFF 0x00
 // booster circuit is turn on
-#define LCD_BOOST_ON            0x04
+#define LCD_BOOST_ON 0x04
 // booster circuit is turn off
-#define LCD_BOOST_OFF           0x00
-#define LCD_OSC_122HZ           0x00
-#define LCD_OSC_131HZ           0x01
-#define LCD_OSC_144HZ           0x02
-#define LCD_OSC_161HZ           0x03
-#define LCD_OSC_183HZ           0x04
-#define LCD_OSC_221HZ           0x05
-#define LCD_OSC_274HZ           0x06
-#define LCD_OSC_347HZ           0x07
+#define LCD_BOOST_OFF 0x00
+#define LCD_OSC_122HZ 0x00
+#define LCD_OSC_131HZ 0x01
+#define LCD_OSC_144HZ 0x02
+#define LCD_OSC_161HZ 0x03
+#define LCD_OSC_183HZ 0x04
+#define LCD_OSC_221HZ 0x05
+#define LCD_OSC_274HZ 0x06
+#define LCD_OSC_347HZ 0x07
 
 // flags Follower control
-#define LCD_FOLLOWER_ON         0x08
-#define LCD_FOLLOWER_OFF        0x00
-#define LCD_RAB_1_00            0x00
-#define LCD_RAB_1_25            0x01
-#define LCD_RAB_1_50            0x02
-#define LCD_RAB_1_80            0x03
-#define LCD_RAB_2_00            0x04
-#define LCD_RAB_2_50            0x05
-#define LCD_RAB_3_00            0x06
-#define LCD_RAB_3_75            0x07
+#define LCD_FOLLOWER_ON 0x08
+#define LCD_FOLLOWER_OFF 0x00
+#define LCD_RAB_1_00 0x00
+#define LCD_RAB_1_25 0x01
+#define LCD_RAB_1_50 0x02
+#define LCD_RAB_1_80 0x03
+#define LCD_RAB_2_00 0x04
+#define LCD_RAB_2_50 0x05
+#define LCD_RAB_3_00 0x06
+#define LCD_RAB_3_75 0x07
 
 class TroykaTextLCD : public Print {
 public:
-    TroykaTextLCD (uint8_t pinBacklight = DEFAULT_PIN_BACKLIGHT, uint8_t i2cAddress = DEFAULT_LCD_I2C_ADDR, TwoWire* wire = &Wire);
-    void begin(uint8_t cols = 16, uint8_t rows = 2, uint8_t charSize = 1);
-    void setContrast(uint8_t contrast);
-    void setBrightness(uint8_t brightness);
-    void setIcon(uint8_t address, uint8_t bit);
-    void clear();
-    void home();
-    void noDisplay();
-    void display();
-    void noBlink();
-    void blink();
-    void noCursor();
-    void cursor();
-    void scrollDisplayLeft();
-    void scrollDisplayRight();
-    void leftToRight();
-    void rightToLeft();
-    void autoscroll();
-    void noAutoscroll();
-    void createChar(uint8_t location, uint8_t charMap[]);
-    void setCursor(uint8_t col, uint8_t row);
-    virtual size_t write(uint8_t value);
-    void command(uint8_t value);
+  TroykaTextLCD(TwoWire *wire = &Wire,
+                uint8_t i2cAddress = DEFAULT_LCD_I2C_ADDR,
+                uint8_t pinBacklight = DEFAULT_PIN_BACKLIGHT);
+  void begin(uint8_t cols = 16, uint8_t rows = 2, uint8_t charSize = 1);
+  void setContrast(uint8_t contrast);
+  void setBrightness(uint16_t brightness);
+  void setIcon(uint8_t address, uint8_t bit);
+  void clear();
+  void home();
+  void noDisplay();
+  void display();
+  void noBlink();
+  void blink();
+  void noCursor();
+  void cursor();
+  void scrollDisplayLeft();
+  void scrollDisplayRight();
+  void leftToRight();
+  void rightToLeft();
+  void autoscroll();
+  void noAutoscroll();
+  void createChar(uint8_t location, uint8_t charMap[]);
+  void setCursor(uint8_t col, uint8_t row);
+  virtual size_t write(uint8_t value);
+  void command(uint8_t value);
 
 private:
-    void setNormalFunction();
-    void setExtendFunction();
-    void send(uint8_t value, uint8_t mode);
-    TwoWire* _wire;
-    uint8_t _i2cAddress;
-    uint8_t _displayFunction;
-    uint8_t _displayControl;
-    uint8_t _displayMode;
-    uint8_t _initialized;
-    uint8_t _rows;
-    uint8_t _cols;
-    uint8_t _charSize;
-    uint8_t _pinBacklight;
+  void setNormalFunction();
+  void setExtendFunction();
+  void send(uint8_t value, uint8_t mode);
+  TwoWire *_wire;
+  uint8_t _i2cAddress;
+  uint8_t _displayFunction;
+  uint8_t _displayControl;
+  uint8_t _displayMode;
+  uint8_t _initialized;
+  uint8_t _rows;
+  uint8_t _cols;
+  uint8_t _charSize;
+  uint8_t _pinBacklight;
 };
 
-#endif  // __TROYKA_TEXT_LCD_H__
+#endif // __TROYKA_TEXT_LCD_H__
