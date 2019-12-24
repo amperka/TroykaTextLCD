f42948435ee7dce13f2fcddc0f29e29e957adedf
Igor <igor@amperka.ru>
2019-12-24 15:53:27 +0300

    replace define to constexpr

--- src/TroykaTextLCD.h
+++ src/TroykaTextLCD.h
@@ -6,96 +6,96 @@
 #include <Wire.h>
 #include <stdint.h>
 
-#define DEFAULT_LCD_I2C_ADDR 0x3E
-#define DEFAULT_PIN_BACKLIGHT 7
+constexpr uint8_t DEFAULT_LCD_I2C_ADDR = 0x3E;
+constexpr uint8_t DEFAULT_PIN_BACKLIGHT = 7;
 
 // commands
-#define LCD_CLEARDISPLAY 0x01
-#define LCD_RETURNHOME 0x02
-#define LCD_ENTRYMODESET 0x04
-#define LCD_DISPLAYCONTROL 0x08
-#define LCD_CURSORSHIFT 0x10
-#define LCD_FUNCTIONSET 0x20
-#define LCD_SETCGRAMADDR 0x40
-#define LCD_SETDDRAMADDR 0x80
+constexpr uint8_t LCD_CLEARDISPLAY = 0x01;
+constexpr uint8_t LCD_RETURNHOME = 0x02;
+constexpr uint8_t LCD_ENTRYMODESET = 0x04;
+constexpr uint8_t LCD_DISPLAYCONTROL = 0x08;
+constexpr uint8_t LCD_CURSORSHIFT = 0x10;
+constexpr uint8_t LCD_FUNCTIONSET = 0x20;
+constexpr uint8_t LCD_SETCGRAMADDR = 0x40;
+constexpr uint8_t LCD_SETDDRAMADDR = 0x80;
 
 // flags for display entry mode (LCD_ENTRYMODESET)
-#define LCD_ENTRYRIGHT 0x00
-#define LCD_ENTRYLEFT 0x02
-#define LCD_ENTRYSHIFTINCREMENT 0x01
-#define LCD_ENTRYSHIFTDECREMENT 0x00
+constexpr uint8_t LCD_ENTRYRIGHT = 0x00;
+constexpr uint8_t LCD_ENTRYLEFT = 0x02;
+constexpr uint8_t LCD_ENTRYSHIFTINCREMENT = 0x01;
+constexpr uint8_t LCD_ENTRYSHIFTDECREMENT = 0x00;
 
 // flags for display on/off control (LCD_DISPLAYCONTROL)
-#define LCD_DISPLAYON 0x04
-#define LCD_DISPLAYOFF 0x00
-#define LCD_CURSORON 0x02
-#define LCD_CURSOROFF 0x00
-#define LCD_BLINKON 0x01
-#define LCD_BLINKOFF 0x00
+constexpr uint8_t LCD_DISPLAYON = 0x04;
+constexpr uint8_t LCD_DISPLAYOFF = 0x00;
+constexpr uint8_t LCD_CURSORON = 0x02;
+constexpr uint8_t LCD_CURSOROFF = 0x00;
+constexpr uint8_t LCD_BLINKON = 0x01;
+constexpr uint8_t LCD_BLINKOFF = 0x00;
 
 // flags for display/cursor shift (LCD_CURSORSHIFT)
-#define LCD_DISPLAYMOVE 0x08
-#define LCD_CURSORMOVE 0x00
-#define LCD_MOVERIGHT 0x04
-#define LCD_MOVELEFT 0x00
+constexpr uint8_t LCD_DISPLAYMOVE = 0x08;
+constexpr uint8_t LCD_CURSORMOVE = 0x00;
+constexpr uint8_t LCD_MOVERIGHT = 0x04;
+constexpr uint8_t LCD_MOVELEFT = 0x00;
 
 // flags for function set (LCD_FUNCTIONSET)
-#define LCD_8BITMODE 0x10
-#define LCD_4BITMODE 0x00
-#define LCD_2LINE 0x08
-#define LCD_1LINE 0x00
-#define LCD_5x10DOTS 0x04
-#define LCD_5x8DOTS 0x00
+constexpr uint8_t LCD_8BITMODE = 0x10;
+constexpr uint8_t LCD_4BITMODE = 0x00;
+constexpr uint8_t LCD_2LINE = 0x08;
+constexpr uint8_t LCD_1LINE = 0x00;
+constexpr uint8_t LCD_5x10DOTS = 0x04;
+constexpr uint8_t LCD_5x8DOTS = 0x00;
 // Normal or Extension instruction select
-#define LCD_NM_INSTRUCTION 0x00
-#define LCD_EX_INSTRUCTION 0x01
+constexpr uint8_t LCD_NM_INSTRUCTION = 0x00;
+constexpr uint8_t LCD_EX_INSTRUCTION = 0x01;
 
 // bias selection / Internal OSC frequency adjust
-#define LCD_EX_SETBIASOSC 0x10
+constexpr uint8_t LCD_EX_SETBIASOSC = 0x10;
 // set ICON RAM address
-#define LCD_EX_SETICONRAMADDR 0x40
+constexpr uint8_t LCD_EX_SETICONRAMADDR = 0x40;
 // power / ICON control / Contrast set (high byte)
-#define LCD_EX_POWICONCONTRASTH 0x50
+constexpr uint8_t LCD_EX_POWICONCONTRASTH = 0x50;
 // follower control
-#define LCD_EX_FOLLOWERCONTROL 0x60
+constexpr uint8_t LCD_EX_FOLLOWERCONTROL = 0x60;
 // contrast set (low byte)
-#define LCD_EX_CONTRASTSETL 0x70
+constexpr uint8_t LCD_EX_CONTRASTSETL = 0x70;
 
 // flags for Bias selection
 // bias will be 1/4
-#define LCD_BIAS_1_4 0x08
+constexpr uint8_t LCD_BIAS_1_4 = 0x08;
 // bias will be 1/5
-#define LCD_BIAS_1_5 0x00
+constexpr uint8_t LCD_BIAS_1_5 = 0x00;
 
 // flags Power / ICON control / Contrast set(high byte)
 // ICON display on
-#define LCD_ICON_ON 0x08
+constexpr uint8_t LCD_ICON_ON = 0x08;
 // ICON display off
-#define LCD_ICON_OFF 0x00
+constexpr uint8_t LCD_ICON_OFF = 0x00;
 // booster circuit is turn on
-#define LCD_BOOST_ON 0x04
+constexpr uint8_t LCD_BOOST_ON = 0x04;
 // booster circuit is turn off
-#define LCD_BOOST_OFF 0x00
-#define LCD_OSC_122HZ 0x00
-#define LCD_OSC_131HZ 0x01
-#define LCD_OSC_144HZ 0x02
-#define LCD_OSC_161HZ 0x03
-#define LCD_OSC_183HZ 0x04
-#define LCD_OSC_221HZ 0x05
-#define LCD_OSC_274HZ 0x06
-#define LCD_OSC_347HZ 0x07
+constexpr uint8_t LCD_BOOST_OFF = 0x00;
+constexpr uint8_t LCD_OSC_122HZ = 0x00;
+constexpr uint8_t LCD_OSC_131HZ = 0x01;
+constexpr uint8_t LCD_OSC_144HZ = 0x02;
+constexpr uint8_t LCD_OSC_161HZ = 0x03;
+constexpr uint8_t LCD_OSC_183HZ = 0x04;
+constexpr uint8_t LCD_OSC_221HZ = 0x05;
+constexpr uint8_t LCD_OSC_274HZ = 0x06;
+constexpr uint8_t LCD_OSC_347HZ = 0x07;
 
 // flags Follower control
-#define LCD_FOLLOWER_ON 0x08
-#define LCD_FOLLOWER_OFF 0x00
-#define LCD_RAB_1_00 0x00
-#define LCD_RAB_1_25 0x01
-#define LCD_RAB_1_50 0x02
-#define LCD_RAB_1_80 0x03
-#define LCD_RAB_2_00 0x04
-#define LCD_RAB_2_50 0x05
-#define LCD_RAB_3_00 0x06
-#define LCD_RAB_3_75 0x07
+constexpr uint8_t LCD_FOLLOWER_ON = 0x08;
+constexpr uint8_t LCD_FOLLOWER_OFF = 0x00;
+constexpr uint8_t LCD_RAB_1_00 = 0x00;
+constexpr uint8_t LCD_RAB_1_25 = 0x01;
+constexpr uint8_t LCD_RAB_1_50 = 0x02;
+constexpr uint8_t LCD_RAB_1_80 = 0x03;
+constexpr uint8_t LCD_RAB_2_00 = 0x04;
+constexpr uint8_t LCD_RAB_2_50 = 0x05;
+constexpr uint8_t LCD_RAB_3_00 = 0x06;
+constexpr uint8_t LCD_RAB_3_75 = 0x07;
 
 class TroykaTextLCD : public Print {
 public:
